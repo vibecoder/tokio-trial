c3c0aa3a8c2c929d02f7785a30f4baf81fb8c99e
Vibhas Goyal <vibhas.goyal@oracle.com>
2023-09-05 17:01:28 +0530

    auto commit soon?

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,6 @@ fn main() {
     simple_logger::init_with_level(Level::Info).unwrap();
     let rt = tokio::runtime::Runtime::new().unwrap();
     let future = run();
+
+    rt.block_on(future);
 }
