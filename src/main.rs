4cd60d0ff3884f10328c080579742852ed8ff6e5
Vibhas Goyal <vibhas.goyal@oracle.com>
2023-09-05 17:27:49 +0530

    new

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
-use tokio::time;
-
 use log::Level;
+use tokio::io::AsyncReadExt;
 
 async fn run() {
-    log::info!("Sleeping");
-    time::sleep(time::Duration::from_secs(1)).await;
+    tokio::join!(
+        sleeper(),
+        reader(),
+        reader(),
+        reader(),
+        reader(),
+        reader(),
+        reader(),
+        reader(),
+        reader(),
+        reader(),
+        writer(),
+    );
+}
+
+async fn reader() {
+    log::info!("This is the reader function");
+    let mut f = tokio::fs::File::open("beeg.csv").await.unwrap();
+    let mut contents = vec![];
+    f.read_to_end(&mut contents).await.unwrap();
+    log::info!("Read beeg {} bytes", contents.len());
+}
+
+async fn sleeper() {
+    log::info!("This is the sleeper function");
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
     log::info!("Awake");
 }
 
-fn main() {
-    simple_logger::init_with_level(Level::Info).unwrap();
-    let rt = tokio::runtime::Runtime::new().unwrap();
-    let future = run();
+async fn writer() {
+    log::info!("This is the writer function");
+}
 
-    rt.block_on(future);
+#[tokio::main]
+async fn main() {
+    simple_logger::init_with_level(Level::Info).unwrap();
+    let start = std::time::Instant::now();
+    run().await;
+    let end = std::time::Instant::now();
+
+    println!("Took {:?} seconds", end - start);
 }
